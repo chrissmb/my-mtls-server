cb57f3ff31b765fe17121ba702b0e8986cff25db
Christopher Monteiro <chrissantista@gmail.com>
2024-02-08 19:11:07 -0300

    first commit

---
+++ src/main/java/com/example/mymtlsserver/MyMtlsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymtlsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMtlsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMtlsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mymtlsserver/MyMtlsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymtlsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMtlsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
